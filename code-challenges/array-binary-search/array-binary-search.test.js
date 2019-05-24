03554e9c02a71070b772d918202aba846824580c
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-05-24 09:31:41 -0500

    added tests up to one million to check any size array

--- code-challenges/array-binary-search/array-binary-search.test.js
+++ code-challenges/array-binary-search/array-binary-search.test.js
@@ -22,8 +22,36 @@
       }
 
 describe('Binary Search', () => {
-    it('Should fnd the selected value in the array', () => {
+    it('Should find the selected value in a small array', () => {
         let arr = [1,2,3,4,5]
         expect(binarySearch(arr, 2)).toBe(1);
+    });
+    it('should find the value in an array of 100', () => {
+      let arr = [];
+        for(let i = 0; i < 101; i++){
+          arr.push(i);
+      }
+      expect(binarySearch(arr, 73)).toBe(73);
+    });
+    it('should find the value in an array of 1000', () => {
+      let arr = [];
+        for(let i = 0; i < 1001; i++){
+          arr.push(i);
+      }
+      expect(binarySearch(arr, 862)).toBe(862);
+    });
+    it('should find the value in an array of 10000', () => {
+      let arr = [];
+        for(let i = 0; i < 10001; i++){
+          arr.push(i);
+      }
+      expect(binarySearch(arr, 1004)).toBe(1004);
+    });
+    it('should find the value in an array of 1000000', () => {
+      let arr = [];
+        for(let i = 0; i < 1000001; i++){
+          arr.push(i);
+      }
+      expect(binarySearch(arr, 1000000)).toBe(1000000);
     });
 });
