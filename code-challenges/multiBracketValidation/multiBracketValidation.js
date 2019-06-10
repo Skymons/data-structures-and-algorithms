a5af74b60a9a604c8e8ccabe710c378d475f59fe
Keith Dahlby <keith@newbo.co>
2019-06-09 22:18:36 -0500

    Import existing Stack; export function only

--- code-challenges/multiBracketValidation/multiBracketValidation.js
+++ code-challenges/multiBracketValidation/multiBracketValidation.js
@@ -1,81 +1,24 @@
-'use strcit';
+'use strict';
 
-class StackNode {
-  constructor(value)
-  {
-    this.value = value;
-    this.next = null;
-  }
-}
+const { Stack } = require('../stacks-and-queues/stacks-and-queues');
 
-class Stack {
-  constructor()
-  {
-    this.top = null;
-  }
+module.exports = function bracketValidation(str) {
+  var brackets = ['(', ')', '[', ']', '{', '}'];
+  var openBrackets = ['(', '{', '['];
+  var closeBrackets = [')', '}', ']'];
+  var splitArr = str.split('').filter(item => {
+    brackets.includes(item);
+  });
 
-  push(value) {
-    if(this.top === null){
-      this.top = new StackNode(value);
+  for(let i = 0; i < splitArr.length; i++) {
+    if(openBrackets.includes(splitArr[i])) {
+      Stack.push(splitArr[i]);
+    }
+    if(closeBrackets.includes(splitArr[i]) && Stack.top.value === openBrackets[closeBrackets.indexOf(splitArr[i])]) {
+      Stack.pop();
     } else {
-      let oldTop = this.top;
-      this.top = new StackNode(value);
-      this.top.next = oldTop;
+      return false;
     }
   }
-
-  pop() {
-    if(this.top === null) {
-      return `Can't pop from empty stack`;
-    } else {
-      let popVal = this.top.value;
-      this.top = this.top.next;
-      return popVal;
-    }
-  }
-
-  peek() {
-    if(this.top === null) {
-      return `Stack is empty`;
-    } else {
-      return this.top.value;
-    }
-  }
-
-  static bracketValidation(str) {
-    var brackets = ['(', ')', '[', ']', '{', '}'];
-    var openBrackets = ['(', '{', '['];
-    var closeBrackets = [')', '}', ']'];
-    var splitArr = str.split('').filter(item => {
-      brackets.includes(item);
-    });
-
-    for(let i = 0; i < splitArr.length; i++) {
-      if(openBrackets.includes(splitArr[i])) {
-        Stack.push(splitArr[i]);
-      }
-      if(closeBrackets.includes(splitArr[i]) && Stack.top.value === openBrackets[closeBrackets.indexOf(splitArr[i])]) {
-        Stack.pop();
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  toString() {
-    var myLinkedListString = [];
-    var current = this.top;
-    while (current) {
-      myLinkedListString.push(current.value);
-      current = current.next;
-    }
-    return myLinkedListString.join(',');
-  }
-}
-
-
-
-
-
-module.exports = Stack;
+  return true;
+};
--- code-challenges/multiBracketValidation/multiBracketValidation.test.js
+++ code-challenges/multiBracketValidation/multiBracketValidation.test.js
@@ -1,16 +1,16 @@
 'use strcit';
 
-const BracVal = require('./multiBracketValidation');
+const bracketValidation = require('./multiBracketValidation');
 
 describe('Multi Bracket Validation in a String', () => {
 
   it('should return true with an empty string', () => {
-    let validation = BracVal.bracketValidation('');
+    let validation = bracketValidation('');
     expect(validation).toBeTruthy();
   });
 
   it('should return true with acorrect string', () => {
-    let validation = BracVal.bracketValidation('');
+    let validation = bracketValidation('');
     expect(validation).toBeTruthy();
   });
 });
